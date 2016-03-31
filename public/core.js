7c898f49650f23c594a0e2b1af21878e59880de4
patrickmounir <patrickmounir@gmail.com>
2016-03-31 10:57:29 +0200

    not working

--- public/core.js
+++ public/core.js
@@ -1,1 +1,15 @@
+App = angular.module('HashFork', ['ui.bootstrap', 'ngRoute']);
+
+/**
+ * Angular Routes
+ */
+App.config(function($routeProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/', {
+            templateUrl : '/partials/main.html',
+            controller  : 'mainCtrl'
+        })
+});
 
