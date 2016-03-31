2d8625e50cad5b636d9f6d7d8da678e5ff845973
el3ankaboot <gamal.m.esmail@gmail.com>
2016-03-31 16:22:36 +0300

    the TO and FROM reads from the dynamic data now

--- public/js/dummyFlights.js
+++ public/js/dummyFlights.js
@@ -2,16 +2,14 @@ var app = angular.module('app',[]);
 
 app.controller('flightListCtrl', function ($scope){
 
-
+	$scope.from = "Cairo";
+	$scope.to = "Amsterdam";
 	$scope.flights = [
 		{dateFrom : "01/03/2017" , timeFrom : "7PM", from : "Cairo" , dateTo : "01/03/2017", timeTo : "11PM" , to : "Amsterdam", cost : 200 },
-		{dateFrom : "02/03/2017" , timeFrom : "2AM", from :"Amsterdam" , dateTo : "02/03/2017": , timeTo : "4AM" , to :"Munich"  , cost :50 },
 
 		{dateFrom : "01/03/2017" , timeFrom : "7AM", from : "Cairo" , dateTo : "01/03/2017", timeTo : "11AM" , to : "Amsterdam", cost : 150 },
-		{dateFrom : "01/03/2017" , timeFrom : "3PM", from :"Amsterdam" , dateTo : "01/03/2017": , timeTo : "6PM" , to :"Munich"  , cost :50 },
 
 		{dateFrom : "01/03/2017" , timeFrom : "9AM", from : "Cairo" , dateTo : "01/03/2017", timeTo : "1PM" , to : "Amsterdam", cost : 180 },
-		{dateFrom : "01/03/2017" , timeFrom : "2AM", from :"Amsterdam" , dateTo : "01/03/2017": , timeTo : "4AM" , to :"Munich"  , cost :40 },
 
 
 
@@ -22,7 +20,7 @@ app.controller('flightListCtrl', function ($scope){
 		{dateFrom : "06/10/2017" , timeFrom : "1PM" , from : "Amsterdam" , dateTo :"06/10/2016" , timeTo :"6PM" , to : "Cairo" , cost : 200 },
 
 		{dateFrom : "06/10/2017" , timeFrom : "12PM" , from :"Munich" , dateTo :"06/10/2016" , timeTo :"2PM" , to : "Amsterdam" , cost : 70 },
-		{dateFrom : "06/10/2017" , timeFrom : "5PM" , from : "Amsterdam" , dateTo :"06/10/2016" , timeTo :"10PM" , to : "Cairo" , cost : 250 },
+		{dateFrom : "06/10/2017" , timeFrom : "5PM" , from : "Amsterdam" , dateTo :"06/10/2016" , timeTo :"10PM" , to : "Cairo" , cost : 250 }
 		
 
 
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
 var app = require('./app/app');
-app.listen(3000);
-console.log("App is running on port 3000");
+app.listen(3000,function(){
+console.log("App is running on port 3000");  
+});
+
