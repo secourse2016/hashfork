7a2a54d9b5c279f9ecd4cc98b20c2a769e10b66c
patrickmounir <patrickmounir@gmail.com>
2016-03-31 11:31:30 +0200

    The improved app

--- app/app.js
+++ app/app.js
@@ -2,6 +2,6 @@ var express       = require('express');
 var app           = express();
 
 app.use(express.static('public'));
-require('./routes')(app);
+require('./routes/routes')(app);
 
 module.exports = app;
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,3 +1,6 @@
+
+module.exports = function(app,mongo) {
 app.get('*', function(req, res) {
         res.sendfile('./public/index.html');
     });
+};
