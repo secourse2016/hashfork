7b1471aecb720ddb2c17e138f128e682f33bb969
Walid El Hefny <walidtarekelhefny@gmail.com>
2016-03-31 20:27:22 +0200

    Dynamically viewing the names using namesCtrl.js and Angular

--- public/controllers/namesCtrl.js
+++ public/controllers/namesCtrl.js
@@ -1,6 +1,52 @@
 var namesView = angular.module('namesView',[]);
 
-namesView.controller('nCtrl',function($scope)) {
+namesView.controller('nCtrl',['$scope', function($scope) {
+
+$scope.travellers = [
+
+{type: 'Adult',
+counter: 1,
+firstName: 'null',
+middleName: 'null',
+lastName: 'null',
+passport: 'null'},
+
+{type: 'Adult',
+counter: 2,
+firstName: 'null',
+middleName: 'null',
+lastName: 'null',
+passport: 'null'},
+
+{type: 'Adult',
+counter: 3,
+firstName: 'null',
+middleName: 'null',
+lastName: 'null',
+passport: 'null'},
+
+{type: 'Child',
+counter: 1,
+firstName: 'null',
+middleName: 'null',
+lastName: 'null',
+passport: 'null'},
+
+{type: 'Child',
+counter: 2,
+firstName: 'null',
+middleName: 'null',
+lastName: 'null',
+passport: 'null'},
+
+{type: 'Baby',
+counter: 1,
+firstName: 'null',
+middleName: 'null',
+lastName: 'null',
+passport: 'null'},
+
+];
 
 
-});
+}]);
