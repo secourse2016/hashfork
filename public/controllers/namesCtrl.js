08ade742c389d38731325c1bc73a930ed2607554
Walid El Hefny <walidtarekelhefny@gmail.com>
2016-03-31 23:08:22 +0200

    Binded the data of the input values to the controller

--- public/controllers/namesCtrl.js
+++ public/controllers/namesCtrl.js
@@ -2,51 +2,53 @@ var namesView = angular.module('namesView',[]);
 
 namesView.controller('nCtrl',['$scope', function($scope) {
 
+
 $scope.travellers = [
 
 {type: 'Adult',
 counter: 1,
-firstName: 'null',
-middleName: 'null',
-lastName: 'null',
-passport: 'null'},
+firstName: '',
+middleName: '',
+lastName: '',
+passport: ''},
 
 {type: 'Adult',
 counter: 2,
-firstName: 'null',
-middleName: 'null',
-lastName: 'null',
-passport: 'null'},
+firstName: '',
+middleName: '',
+lastName: '',
+passport: ''},
 
 {type: 'Adult',
 counter: 3,
-firstName: 'null',
-middleName: 'null',
-lastName: 'null',
-passport: 'null'},
+firstName: '',
+middleName: '',
+lastName: '',
+passport: ''},
 
 {type: 'Child',
 counter: 1,
-firstName: 'null',
-middleName: 'null',
-lastName: 'null',
-passport: 'null'},
+firstName: '',
+middleName: '',
+lastName: '',
+passport: ''},
 
 {type: 'Child',
 counter: 2,
-firstName: 'null',
-middleName: 'null',
-lastName: 'null',
-passport: 'null'},
+firstName: '',
+middleName: '',
+lastName: '',
+passport: ''},
 
 {type: 'Baby',
 counter: 1,
-firstName: 'null',
-middleName: 'null',
-lastName: 'null',
-passport: 'null'},
+firstName: '',
+middleName: '',
+lastName: '',
+passport: ''},
 
 ];
+
 
 
 }]);
