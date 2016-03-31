021e9552f1e4e1e4201510bd2fb339f099711768
Patrick Mounir <patrickmounir@gmail.com>
2016-03-31 12:35:56 +0300

    tmp services

--- public/services/flightsSrv.js
+++ public/services/flightsSrv.js
@@ -3,20 +3,6 @@
  */
 App.factory('FlightsSrv', function ($http) {
      return {
-         getAirportCodes : function() {
-           return $http.get('/api/data/codes');
-         },
-         setSelectedOriginAirport: function(value) {
-           this.selectedOriginAirport = value;
-         },
-         getSelectedOriginAirport: function() {
-           return this.selectedOriginAirport;
-         },
-         setSelectedDestinationAirport: function(value) {
-           this.selectedDestinationAirport = value;
-         },
-         getSelectedDestinationAirport: function() {
-           return this.selectedDestinationAirport;
-         }
+        
      };
  });
