a024940a72164172c6f4425ef945963b31cf443c
Ahmed Ghazy <a.ghazy500@gmail.com>
2016-03-31 16:16:08 +0200

    Almost Done

--- public/controllers/confirmation.js
+++ public/controllers/confirmation.js
@@ -7,6 +7,17 @@ app1.controller('ctrl1', function($scope) {
   $scope.adultPrice = 100;
   $scope.childPrice = 50;
   $scope.babyPrice = 20;
+  $scope.date1 = "31/03/2016";
+  $scope.date2 = "31/03/2016";
+  $scope.date3 = "05/05/2016";
+  $scope.date4 = "06/05/2016";
+  $scope.time1 = "16:20";
+  $scope.time2 = "19:30";
+  $scope.time3 = "23:20";
+  $scope.time4 = "02:20";
+  $scope.from = "Cairo, Egypt";
+  $scope.to = "Dubai, United Arab Emirates";
+
 
   $scope.total = $scope.adultNo*$scope.adultPrice + $scope.childNo*$scope.childPrice + $scope.babyNo*$scope.babyPrice;
 
