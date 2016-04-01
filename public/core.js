6c71353354e3d1edba5d29320123530b0591fc74
Walid El Hefny <walidtarekelhefny@gmail.com>
2016-04-01 15:00:43 +0200

    Adding new feature to disable buttons if required fields not met, also implementing the signin and logout ng-clicks

--- public/core.js
+++ public/core.js
@@ -3,6 +3,15 @@ App = angular.module('HashFork', ['ui.bootstrap', 'ngRoute']);
 /**
  * Angular Routes
  */
+
+App.controller('ctrl',['$scope', function($scope) {
+
+  $scope.user =
+    {name: '',
+    email: ''};
+
+  }]);
+
 App.config(function($routeProvider) {
     $routeProvider
 
@@ -12,4 +21,3 @@ App.config(function($routeProvider) {
             controller  : 'mainCtrl'
         })
 });
-
