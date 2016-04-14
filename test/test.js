5447ce314a1f9c6d937a8e7f02dbf5d13afdef38
ahmedouda1995 <ahmedouda.1995@hotmail.com>
2016-04-14 11:59:50 +0200

    added 2 tests for api/flights and api/airports

--- test/test.js
+++ test/test.js
@@ -1,13 +1,44 @@
 var assert = require('chai').assert;
 var request = require('supertest');
+var db = require('../app/db.js');
+var routes = require('../app/routes/routes.js');
 
- describe('Array', function() {
-	describe('#indexOf()', function() {
-		it('should return -1 when the value is not present', function() {
-			assert.equal(true, true);
-			assert.equal(false, false);
-		});
-	});
+
+//  describe('Array', function() {
+// 	describe('#indexOf()', function() {
+// 		it('should return -1 when the value is not present', function() {
+// 			assert.equal(true, true);
+// 			assert.equal(false, false);
+// 		});
+// 	});
+// });
+
+describe('API', function() {
+    request = request(routes);
+    
+    it('/api/airports should return a JSON array of all airports', function(done) {
+        request
+       .get('/api/airports')
+       .set('Accept', 'application/json')
+       .expect(200)
+       .end(function(err,res) {
+         assert.isArray(res.body, 'should be JSON array');
+         assert.equal(res.body.length,102,'JSON  array should have 6726 airports');
+         done();
+     });
+    });
+
+    it('/api/flights should return a JSON array of all flights', function(done) {
+        request
+       .get('/api/flights')
+       .set('Accept', 'application/json')
+       .expect(200)
+       .end(function(err,res) {
+         assert.isArray(res.body, 'should be JSON array');
+         assert.equal(res.body.length,3000,'JSON  array should have 3000 flights');
+         done();});
+   });
+
 });
 
 
