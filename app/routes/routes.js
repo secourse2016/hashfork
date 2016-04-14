2c2248a310fc8a808ee2efe3ac6bd0fe9e5b3ba9
Walid El Hefny <walidtarekelhefny@gmail.com>
2016-04-14 10:06:07 +0200

    First Commit, adding required files

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,6 +1,8 @@
 
 module.exports = function(app) {
 
+	var jwt = require('jsonwebtoken');
+
 	app.get('/api/airports',function(req,res){
 		var airports =  require('../../airports.json');
 		res.json(airports);
@@ -15,6 +17,13 @@ module.exports = function(app) {
 	console.log("request reeceived");
         res.sendfile('./public/index.html');
     });
+
+		/* Middlewear for securing the APIs */
+
+		app.use(function(req, res, next) {
+
+
+
+
+			});
 };
-
-
