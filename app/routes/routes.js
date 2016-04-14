4241209c7c0bf716b72110d519eaa5cb4a6cf81d
Walid El Hefny <walidtarekelhefny@gmail.com>
2016-04-14 11:04:18 +0200

    Adding the 403.html page, and adding the condition in core.js

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -3,6 +3,8 @@ module.exports = function(app) {
 
 	var jwt = require('jsonwebtoken');
 
+	// Unsecured Part
+
 	app.get('/api/airports',function(req,res){
 		var airports =  require('../../airports.json');
 		res.json(airports);
@@ -22,11 +24,10 @@ module.exports = function(app) {
 
 		app.use(function(req, res, next) {
 
-
 			var token = req.body.wt || req.query.wt || req.headers['x-access-token'];
 
 			var secret = process.env.JWTSECRET;
-
+			//Trying to verify, if failed throws an error.
 			try
       {
         var payload = jwt.verify(token, secret);
@@ -35,9 +36,14 @@ module.exports = function(app) {
       }
       catch (err)
       {
-        res.status(403).sendFile(path.join(__dirname, '../public', '403.html'));
+        res.status(403).sendFile('../public/index.html/403');
+
       }
 
 
 			});
+
+			// SECURED Part
+
+
 };
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 
 var app = require('./app/app');
 
-app.listen(3000,function(err){
+app.listen(process.env.PORT,function(err){
 	if(err){
 		console.log("This is an error");
 	}else{
-		console.log("App is running on port 80");		
+		console.log("App is running on port 80");
 	}
-  
+
 
 });
-
