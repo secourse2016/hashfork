41dd2b32d6e612091699e774386c7416ff2cae2f
Mina <mina.samaan@student.guc.edu.eg>
2016-04-14 10:10:13 +0200

    added seed function

--- app/db.js
+++ app/db.js
@@ -1,6 +1,8 @@
 var mongo = require('mongodb').MongoClient;
 var DB = null;
 var dbURL = 'mongodb://localhost:27017/klm';
+var Airports = require('../airports.json');
+var Flights = require('../flights.json');
 
 
 
@@ -13,4 +15,35 @@ exports.connect = function(cb) {
     });
 };
 
-//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+function seed(cb) {
+   DB.collection('Airports').find().toArray(function (err, docs) {
+        if (err) return cb(err);
+        if (docs.length > 0)
+            cb(null, false);
+        else {
+            DB.collection('Airports').insertMany(Airports, function (err) {
+                if (err) return cb(err);
+                cb(null, true);
+            });
+        }
+    });
+
+     DB.collection('Flights').find().toArray(function (err, docs) {
+        if (err) return cb(err);
+        if (docs.length > 0)
+            cb(null, false);
+        else {
+            db.db().collection('Flights').insertMany(Flights, function (err) { 
+                if (err) return cb(err);
+                cb(null, true);
+            });
+        }
+    });
+}
