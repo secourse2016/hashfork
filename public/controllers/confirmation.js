b446d3ad845f4cfdd7553bf83930c90b4efbb868
patrickmounir <patrickmounir@gmail.com>
2016-04-17 21:02:29 +0200

    The one way issue in confirmation is resolved

--- public/controllers/confirmation.js
+++ public/controllers/confirmation.js
@@ -4,24 +4,29 @@ App.controller('confirmation', function($scope,FlightsSrv,$location) {
   $scope.adultNo = FlightsSrv.getAdults();
   $scope.childNo = FlightsSrv.getChild();
   $scope.babyNo = FlightsSrv.getBaby();
+   if(FlightsSrv.isReturn() === true){
+  $scope.returnFlight = 1;
   $scope.adultPrice = Number(flight.outgoingFlights.cost)+Number(flight.returnFlights.cost);
+  $scope.date3 =  moment(flight.returnFlights.departureDateTime).format('MM-DD-YYYY');
+  $scope.date4 = moment(flight.returnFlights.arrivalDateTime).format('MM-DD-YYYY');
+  $scope.time3 = moment(flight.returnFlights.departureDateTime).format('hh:mm');
+  $scope.time4 = moment(flight.returnFlights.arrivalDateTime).format('hh:mm');
+}else{
+  $scope.returnFlight=0;
+  $scope.adultPrice = Number(flight.outgoingFlights.cost)
+}
+  
   $scope.childPrice = ($scope.adultPrice)/2;
   $scope.babyPrice = ($scope.adultPrice)/4;
   $scope.date1 = moment(flight.outgoingFlights.departureDateTime).format('MM-DD-YYYY');
   $scope.date2 = moment(flight.outgoingFlights.arrivalDateTime).format('MM-DD-YYYY');
-  $scope.date3 =  moment(flight.returnFlights.departureDateTime).format('MM-DD-YYYY');
-  $scope.date4 = moment(flight.returnFlights.arrivalDateTime).format('MM-DD-YYYY');
+  
   $scope.time1 = moment(flight.outgoingFlights.departureDateTime).format('hh:mm');
   $scope.time2 = moment(flight.outgoingFlights.arrivalDateTime).format('hh:mm');
-  $scope.time3 = moment(flight.returnFlights.departureDateTime).format('hh:mm');
-  $scope.time4 = moment(flight.returnFlights.arrivalDateTime).format('hh:mm');
+  
   $scope.from = flight.outgoingFlights.origin;
   $scope.to = flight.outgoingFlights.destination;
-  if(FlightsSrv.isReturn() === true){
-  $scope.returnFlight = 1;
-}else{
-  $scope.returnFlight=0;
-}
+ 
 
   $scope.total = $scope.adultNo*$scope.adultPrice + $scope.childNo*$scope.childPrice + $scope.babyNo*$scope.babyPrice;
   FlightsSrv.setCost($scope.total)
