5eae965396f8eb3fe313f0aab161f055587cd99c
patrickmounir <patrickmounir@gmail.com>
2016-04-18 11:18:51 +0200

    The option method

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
 	var db = require('../db');
 	// Unsecured Part
 	app.use(require('body-parser').json());
-	app.all('*', function(req, res, next) {
+	app.use('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods','PUT,GET,POST,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
