af604c9868d762e790775bd6ea2c8e68f17f41f9
patrickmounir <patrickmounir@gmail.com>
2016-04-18 11:29:50 +0200

    app

--- app/app.js
+++ app/app.js
@@ -1,7 +1,8 @@
 var express       = require('express');
 var bodyParser    = require('body-parser');
+var cors = require('cors');
 var app           = express();
-
+app.use(cors());
 
 // app.use(express.static('public'));
 require('dotenv').load();
