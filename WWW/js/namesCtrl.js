310c8c81fd1f3046c44d050de8dd69ac059cc690
Walid El Hefny <walidtarekelhefny@gmail.com>
2016-04-27 13:43:32 +0200

    Adding some missing pages

---
+++ WWW/js/namesCtrl.js
@@ -0,0 +1,64 @@
+angular.module('app.namesCtrl', [])
+
+.controller('nCtrl',function($scope, $state) {
+
+$scope.goNext = function(){
+  //console.log("dosna");
+  $state.go('payment'); 
+}
+});
+
+
+// $scope.travellers = [];
+// 	var adults=FlightsSrv.getAdults();
+// 	var child =FlightsSrv.getChild();
+// 	var baby =FlightsSrv.getBaby();
+// 	var i =1;
+// 	while(i<=adults){
+// 		var entry={};
+// 		entry.type='Adult';
+// 		entry.counter=i;
+// 		entry.firstName='';
+// 		entry.middleName='';
+// 		entry.lastName='';
+// 		entry.passport='';
+// 		i++;
+// 		$scope.travellers.push(entry);
+
+// 	}
+// 	var i =1;
+// 	while(i<=child){
+// 		var entry={};
+// 		entry.type='Child';
+// 		entry.counter=i;
+// 		entry.firstName='';
+// 		entry.middleName='';
+// 		entry.lastName='';
+// 		entry.passport='';
+// 		i++;
+// 		$scope.travellers.push(entry);
+
+// 	}	var i =1;
+// 	while(i<=baby){
+// 		var entry={};
+// 		entry.type='Baby';
+// 		entry.counter=i;
+// 		entry.firstName='';
+// 		entry.middleName='';
+// 		entry.lastName='';
+// 		entry.passport='';
+// 		i++;
+// 		$scope.travellers.push(entry);
+
+// 	}
+// $scope.user =
+//   {name: '',
+//   email: ''};
+//   $scope.goToPrev=function(){
+   
+//     $location.url('/confirmation');
+  
+//   };
+//   $scope.goNext=function(){
+//     $location.url('/ref');
+//   };
