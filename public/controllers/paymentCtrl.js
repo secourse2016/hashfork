8f49472b1a54634c3b41a693b01620a78e7e5072
patrickmounir <patrickmounir@gmail.com>
2016-05-04 16:24:08 +0200

    card validation done

--- public/controllers/paymentCtrl.js
+++ public/controllers/paymentCtrl.js
@@ -1,6 +1,8 @@
 App.controller('paymentCtrl',function($scope,FlightsSrv,$location,Stripe){
 
-
+$scope.card =true;
+  	$scope.expiry=true;
+  	$scope.cvcs=true;
 
 $scope.goToPrev=function(){
     
@@ -8,9 +10,15 @@ $scope.goToPrev=function(){
   
   };
   $scope.goNext=function(){
-    FlightsSrv.setCardInfo($scope.cardNumber,$scope.cvc,$scope.expiryMonth,$scope.expiryYear);
+  	$scope.card =Stripe.card.validateCardNumber($scope.cardNumber);
+  	$scope.expiry=Stripe.card.validateExpiry($scope.expiryMonth, $scope.expiryYear);
+  	$scope.cvcs=Stripe.card.validateCVC($scope.cvc);
+  	if($scope.card === true&&$scope.expiry===true&&$scope.cvcs===true){
+  	FlightsSrv.setCardInfo($scope.cardNumber,$scope.cvc,$scope.expiryMonth,$scope.expiryYear);
     $location.url('/ref');
-  
+  	
+  	}   
+    
    
   };
 });
